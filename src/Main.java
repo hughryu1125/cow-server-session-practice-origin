e93dd2f3d88360b6d6238da79de3fce3fe695fdf
hughryu1125 <hughr1125@gmail.com>
2026-03-19 02:40:48 +0900

    Revert "feat: add RandomNumberCreator"
    This reverts commit f102551c906afdaa846be13917b494134aefeee6.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,7 @@
-import java.util.List;
-
 public class Main {
     public static void main(String[] args) {
         // TODO: src에 random한 숫자 6개를 생성하는 RandomNumberCreator 만들고
         //  main에서 출력하기
         //  RandomNumberCreator의 return type은 Array, List 뭐든 상관없습니다!
-        RandomNumberCreator randomNumberCreator = new RandomNumberCreator();
-        List<Integer> randomNumbers = randomNumberCreator.createRandomNumbers();
-
-        for (int number : randomNumbers) {
-            System.out.println(number);
-        }
     }
 }
